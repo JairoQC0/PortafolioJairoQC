b9b4e48bc16ec857e8048906c8765a9326baa64f
JairoQC <jairoqc.02@gmail.com>
2025-07-24 18:33:33 -0500

    change icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -524,6 +524,110 @@ function Skills() {
         </svg>
       ),
     },
+    {
+      nombre: "Kotlin",
+      icono: (
+        <svg
+          viewBox="0 0 256 256"
+          width="20"
+          height="20"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="xMidYMid"
+        >
+          <defs>
+            <linearGradient
+              x1="99.991%"
+              y1="-.011%"
+              x2=".01%"
+              y2="100.01%"
+              id="kotlin__a"
+            >
+              <stop stop-color="#E44857" offset=".344%" />
+              <stop stop-color="#C711E1" offset="46.89%" />
+              <stop stop-color="#7F52FF" offset="100%" />
+            </linearGradient>
+          </defs>
+          <path fill="url(#kotlin__a)" d="M256 256H0V0h256L128 127.949z" />
+        </svg>
+      ),
+    },
+    {
+      nombre: "Flutter",
+      icono: (
+        <svg
+          width="20"
+          height="20"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="xMidYMid"
+          viewBox="0 0 256 317"
+        >
+          <defs>
+            <linearGradient
+              x1="4%"
+              y1="27%"
+              x2="75.9%"
+              y2="52.9%"
+              id="flutter__a"
+            >
+              <stop offset="0%" />
+              <stop stop-opacity="0" offset="100%" />
+            </linearGradient>
+          </defs>
+          <path
+            fill="#47C5FB"
+            d="M158 0 0 158l49 48L255 0zM157 145l-85 85 49 50 49-49 85-86z"
+          />
+          <path fill="#00569E" d="m121 280 37 37h97l-85-86z" />
+          <path fill="#00B5F8" d="m72 230 48-48 50 49-49 49z" />
+          <path
+            fill-opacity=".8"
+            fill="url(#flutter__a)"
+            d="m121 280 41-14 4-31z"
+          />
+        </svg>
+      ),
+    },
+    {
+      nombre: "Swift",
+      icono: (
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 32 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M22.1356 25.2715C18.835 27.17 14.2967 27.3651 9.73079 25.4166C6.03387 23.8505 2.96639 21.109 1 17.9767C1.94387 18.7598 3.04503 19.3865 4.22484 19.9344C8.9401 22.135 13.6544 21.9843 16.972 19.94C12.2526 16.3374 8.23626 11.6326 5.2475 7.79547C4.61799 7.16887 4.14605 6.38567 3.67412 5.68084C7.29225 8.97014 13.0343 13.1207 15.0789 14.296C10.7535 9.75321 6.8991 4.1147 7.0561 4.27103C13.8993 11.163 20.2707 15.079 20.2707 15.079C20.4815 15.1974 20.6442 15.2959 20.775 15.384C20.9129 15.0347 21.0338 14.672 21.1357 14.296C22.2368 10.3018 20.9787 5.759 18.2254 2C24.5962 5.83752 28.3721 13.0425 26.7985 19.0732C26.7575 19.2359 26.7129 19.3965 26.6648 19.5542C29.8106 23.4703 29.0008 27.6884 28.6076 26.9053C26.901 23.5801 23.7418 24.5969 22.1356 25.2715Z"
+              fill="url(#paint0_linear_87_8231)"
+            ></path>{" "}
+            <defs>
+              {" "}
+              <linearGradient
+                id="paint0_linear_87_8231"
+                x1="15.0103"
+                y1="2"
+                x2="15.0103"
+                y2="27.0013"
+                gradientUnits="userSpaceOnUse"
+              >
+                {" "}
+                <stop stop-color="#F88A36"></stop>{" "}
+                <stop offset="1" stop-color="#FD2020"></stop>{" "}
+              </linearGradient>{" "}
+            </defs>{" "}
+          </g>
+        </svg>
+      ),
+    },
   ];
 
   return (
